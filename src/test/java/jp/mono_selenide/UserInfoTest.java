79082af2ab60a78766d97e770c0e81c0e3577dcf
rakyooooo <yff20653@gmail.com>
2022-09-07 23:11:18 +0900

    [update]CSSSelectorの改善

--- src/test/java/jp/mono_selenide/UserInfoTest.java
+++ src/test/java/jp/mono_selenide/UserInfoTest.java
@@ -37,7 +37,7 @@ public class UserInfoTest {
         $(By.name("user-id")).val("10");
         $(By.name("user-name")).val("selenide-name");
         sleep(1000);
-        $(By.name("do-insert-button")).click();
+        $(By.className("action-button")).$(By.name("do-insert-button")).click();
         sleep(1000);
         $(By.className("nav-bar")).$(By.name("link-SearchUser")).click();
         sleep(1000);
@@ -45,8 +45,8 @@ public class UserInfoTest {
         // 参照
         $(By.className("nav-bar")).$(By.name("link-SearchUser")).click();
         sleep(1000);
-        $(By.name("search-user-id")).val("10");
-        $(By.name("search-user-name")).val("selenide-name");
+        $(By.className("searching-option")).$(By.name("search-user-id")).val("10");
+        $(By.className("searching-option")).$(By.name("search-user-name")).val("selenide-name");
         sleep(1000);
         $(By.name("do-search-button")).click();
         sleep(1000);
@@ -54,8 +54,8 @@ public class UserInfoTest {
         // 更新
         $(By.name("user-info-10")).click();
         sleep(1000);
-        $(By.name("modify-user-name")).val("update_user_name");
-        $(By.name("do-modify-button")).click();
+        $(By.className("user-info-update")).$(By.name("modify-user-name")).val("update_user_name");
+        $(By.className("action-button")).$(By.name("do-modify-button")).click();
         sleep(1000);
         $(By.className("nav-bar")).$(By.name("link-SearchUser")).click();
         sleep(1000);
